847e71914e1ff1c5d15be3680bda7361b660748c
mcbauman <m.c.baumann@icloud.com>
2024-01-17 13:06:44 +0100

    main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import ImportCSV from './components/ImportCSV.vue';
 </script>
 
 <template>
   <main>
     Hello World
+    <ImportCSV/>
   </main>
 </template>
 
---
+++ frontend/src/components/ImportCSV.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <form>
+        <input type="file">
+        <button type="submit">Import CSV</button>
+    </form>
+</template>
